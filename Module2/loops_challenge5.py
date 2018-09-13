fe7e1956f5d85efa2ea7a7fb22d524b113d9d090
Dominic Pimpinella <dominic@Dominics-MacBook-Pro.local>
2018-09-12 21:26:12 -0400

    Create dictionary_manipulation.py

--- Module2/loops_challenge5.py
+++ Module2/loops_challenge5.py
@@ -5,7 +5,13 @@ keys = list(labels.keys())
 print(keys)
 
 for key in keys:
+
+    # 2 problems: quotes in list of keys, using 'key' as variable in %s string concatenation
+    # print(keys): ['state', 'element', 'alpha']  - quotes will break dictionary access
     k = key.strip("'")
-    keyString = '%('+ key + ')s'
+
+    # print("%(key)s" %(labels)): will not work, will interpret 'key' literally, not as variable from for loop
+    # create keyString to avoid this problem
+    keyString = "%({})s".format(key)
     print(keyString %(labels))
 
