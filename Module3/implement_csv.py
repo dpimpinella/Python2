d162120fb2612a009a9da2804d22ed5452161b1f
Dominic Pimpinella <dom.pimpinella@gmail.com>
2018-09-25 18:08:21 -0400

    read lines into file, total number of inmates

--- Module3/implement_csv.py
+++ Module3/implement_csv.py
@@ -1,9 +1,25 @@
 
 file = open('9_2018_ACJ_daily_census_data.csv')
-header = str(file.readline())
-split_header = header.split(',')
+headers = str(file.readline()).split(',')
+
+num_black = 0
+num_white = 0
+total = 0
+line ="line text"
+
+for x in file:
+        line = str(file.readline()).split(',')
+        line_data =  dict(zip(headers,line))
+        if line_data['Race'] == 'B':
+            num_black += 1
+        elif line_data['Race'] == "W":
+            num_white += 1
+        total += 1
 
 
+print('Number of black inmates:', num_black)
+print('Number of white inmates:', num_white)
+print('Total inmates:', total)
 
 
 
