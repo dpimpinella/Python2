ee9efc89f1ed73d3ac4aaaf9055d083bef02e5c8
Dominic Pimpinella <dom.pimpinella@gmail.com>
2018-10-18 16:49:23 -0400

    Added mechanism for sending Tweets to text-sentiment

--- Module7/tweet_sentiment.py
+++ Module7/tweet_sentiment.py
@@ -8,8 +8,8 @@ class OAuthToken:
     Twitter API requests.
     """
     API_OAUTH_ENDPOINT = 'https://api.twitter.com/oauth2/token'
-    API_CONSUMER_KEY = 'key goes here'
-    API_SECRET_KEY = 'key goes here'
+    API_CONSUMER_KEY = 'bym5ha8MQewR5fEqy2dZwRdBI'
+    API_SECRET_KEY = 'VafZzWm4mRvwhZ4v9wKrgAtqneMhfVrHywjcdCR9auoUJ2JcMa'
 
     def __init__(self):
         self.bearer_token = self.request_bearer_token()
@@ -17,8 +17,8 @@ class OAuthToken:
     def request_bearer_token(self):
         key_secret = '{}:{}'.format(
             OAuthToken.API_CONSUMER_KEY,
-            OAuthToken.API_SECRET_KEY).encode('ascii')
-        b64_encoded_key = base64.b64encode(key_secret)
+            OAuthToken.API_SECRET_KEY)
+        b64_encoded_key = base64.b64encode(key_secret.encode('ascii'))
         b64_encoded_key = b64_encoded_key.decode('ascii')
 
         headers = {
@@ -36,35 +36,75 @@ class OAuthToken:
         return bearer_token
 
 class TwitterSearch:
-    """Encodes query to Twitter Search API and stores results.
+    """UTF-8 and URL encodes query to Twitter Search API and stores the 
+    resulting text from the Tweets in a list.
     """
     API_SEARCH_ENDPOINT = 'https://api.twitter.com/1.1/search/tweets.json?q='
 
-    def __init__(self, query):
+    def __init__(self, query, token):
         self.query = query
-        self.results = self.sendSearch()
-        self.tweet_text = self.findText()
+        self.token = token
+        self.search_results = self.send_search()
+        self.tweet_text = self.find_text()
 
-    def sendSearch(self):
+    def send_search(self):
         encoded_query = urllib.parse.quote(self.query.encode('utf-8'))
         url = self.API_SEARCH_ENDPOINT + encoded_query
         headers = {
             'User-Agent':'TemperatureOfTheRoom',
-            'Authorization':'Bearer ' + OAuthToken().bearer_token,
+            'Authorization':'Bearer ' + token.bearer_token,
             'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'}
         response = requests.get(url, headers = headers)
         results = json.loads(response.content)
         return results
     
-    def findText(self):
+    def find_text(self):
         tweet_text = []
-        for item in self.results["statuses"]:
-            tweet_text.append(item["text"])
+        for item in self.search_results['statuses']:
+            tweet_text.append(item['text'])
         return tweet_text
 
-search = TwitterSearch("roses-filter:retweets-filter:replies")
+class SentimentAnalysis:
+    """Sends Tweet text to http://text-processing.com/api/sentiment/ and stores
+    the response.
+    """
+    SENTIMENT_API_ENDPOINT = 'http://text-processing.com/api/sentiment/'
 
-for item in search.tweet_text:
-    print(item)
+    def __init__(self, tweet_text):
+        self.tweet_text = tweet_text
+        self.sentiment_results = self.total_results()
+
+    def send_tweet_text(self, text):
+
+        headers = {
+            'X-Mashape-Key':'Zx3Hnd9BrvmshEBx4i5UfCZlJqHKp1ddegSjsnmdXY62V9Ndsh',
+			'Content-Type':'application/x-www-form-urlencoded',
+			'Accept':'application/json'}
+        body = {'language':'english', "text":text}
+        response = requests.post(
+            SentimentAnalysis.SENTIMENT_API_ENDPOINT,
+            data = body,
+            headers = headers)
+        result = json.loads(response.content)
+        result = result['label']
+        return result
+    
+    def total_results(self):
+        results = []
+        for item in self.tweet_text:
+            results.append(self.send_tweet_text(item))
+        return results
+
+
+token = OAuthToken()
+search_results = TwitterSearch("donald trump -filter:retweets -filter:replies", token).tweet_text
+
+
+sentiment = SentimentAnalysis(search_results)
+print(sentiment.sentiment_results)
+
+
+
+
 
 
